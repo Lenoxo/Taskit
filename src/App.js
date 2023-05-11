802559c6f79573918e6f464880ba7479f0ff0fa1
Emanuel Padilla <emanuehl159@gmail.com>
2023-05-10 23:02:39 -0500

    Creo mi primer componente en App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './platzi.webp';
 import './App.css';
 
+// El componente es la función como tal
 function App() {
+  // El return es el valor que retorna el componente
   return (
     <div className="App">
+      {/* Aquí se está instertando un componente (ToDoItem) dentro de este otro componente (App) */}
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,10 +21,21 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendiendo React
         </a>
       </header>
     </div>
+  );
+}
+
+function ToDoItem() {
+  return (
+    <li>
+      {/* Esto es un elemento de React, y todo el return está escrito con sintaxis JSX */}
+      <span>V</span>
+      <p>Limpia la cebolla, y llora con la llorona</p>
+      <span>X</span>
+    </li>
   );
 }
 
