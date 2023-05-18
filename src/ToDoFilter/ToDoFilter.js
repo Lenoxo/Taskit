521f216f3460d9ead130ded7a427fac8b658aa3e
Emanuel Padilla <emanuehl159@gmail.com>
2023-05-17 22:10:58 -0500

    Añado el estado searchValue a ToDoFilter. Se actualiza cuando hacen cambios en el input de ToDoFilter. Avances hasta la clase 7.

--- src/ToDoFilter/ToDoFilter.js
+++ src/ToDoFilter/ToDoFilter.js
@@ -1,11 +1,13 @@
-import './ToDoFilter.css'
+import React, { useState } from 'react';
+import './ToDoFilter.css';
 
 function ToDoFilter() {
+  const [searchValue, setSearchValue] = React.useState();
+  useState('');
+  console.log('Los usuarios buscan todos de ' + searchValue);
   return (
-    <input placeholder="Cortar Cebolla" className='ToDoFilter' onChange={(event) => {
-      console.log('Escribiste en ToDoFilter');
-      console.log(event);
-      console.log(event.target.value);
+    <input placeholder="Cortar Cebolla" className='ToDoFilter' value={searchValue} onChange={(event) => {
+      setSearchValue(event.target.value)
     }}/>
   );
 }
