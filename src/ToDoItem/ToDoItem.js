09c2cbb57b9e8ffcefca556db5d2294524378c68
Emanuel Padilla <emanuehl159@gmail.com>
2023-05-20 01:00:20 -0500

    Importo react-icons para ToDoItem, cambio estilos en AddToDoButton. Avances hasta la clase 11.

--- src/ToDoItem/ToDoItem.js
+++ src/ToDoItem/ToDoItem.js
@@ -1,12 +1,24 @@
-import './ToDoItem.css'
+import "./ToDoItem.css";
+import React from "react";
+// Esta es una forma especial de hacer imports, cambiando el nombre usando as.
+import { BsCheckCircle as CheckIcon } from "react-icons/bs";
+import { BsXCircle as DeleteIcon } from "react-icons/bs";
 
 function ToDoItem(props) {
   return (
-    <li className='ToDoItem'>
-      {/* Esto es un elemento de React, y todo el return está escrito con sintaxis JSX */}
-      <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`} onClick={props.toggleToDoState}>V</span>
-      <p className={`ToDoItem-p ${props.completed && "ToDoItem-p--completed"}`}>{props.text}</p>
-      <span className='Icon Icon-delete' onClick={props.deleteToDo}>X</span>
+    <li className="ToDoItem">
+      <span
+        className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
+        onClick={props.toggleToDoState}
+      >
+        <CheckIcon />
+      </span>
+      <p className={`ToDoItem-p ${props.completed && "ToDoItem-p--completed"}`}>
+        {props.text}
+      </p>
+      <span className='Icon Icon-delete' onClick={props.deleteToDo}>
+        <DeleteIcon />
+      </span>
     </li>
   );
 }
